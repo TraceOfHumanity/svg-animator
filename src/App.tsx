3ce6a76938dbc9bcb43ccc5ceab862e89a405505
Alexander_Ovchar <azalexxx02@gmail.com>
2025-02-13 16:37:19 +0200

    Add SVG download and animation duration control

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { MainContainer } from "./components/MainContainer";
 import { Title } from "./components/Title";
 import { useState } from "react";
+import { saveAs } from "file-saver";
 
 function App() {
   const [svgs, setSvgs] = useState<string[]>([]);
+  const [animationDuration, setAnimationDuration] = useState("2s");
 
   const handleSvgUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -29,15 +31,32 @@ function App() {
       const endTime = ((index + 1) / totalFrames).toFixed(3);
       return `
         <g id="group${index + 1}" opacity="0">${svg}</g>
-        <animate href="#group${index + 1}" attributeName="opacity" values="0;0;1;1;0;0" dur="1s" repeatCount="indefinite" keyTimes="0;${startTime};${startTime};${endTime};${endTime};1" fill="freeze"></animate>
+        <animate href="#group${index + 1}" attributeName="opacity" values="0;0;1;1;0;0" dur="${animationDuration}" repeatCount="indefinite" keyTimes="0;${startTime};${startTime};${endTime};${endTime};1" fill="freeze"></animate>
       `;
     }).join('');
+  };
+
+  const downloadSVG = () => {
+    const svgContent = `
+      <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+        ${generateAnimation()}
+      </svg>
+    `;
+    const blob = new Blob([svgContent], { type: "image/svg+xml" });
+    saveAs(blob, "animated.svg");
   };
 
   return (
     <MainContainer>
       <Title />
       <input type="file" accept="image/svg+xml" multiple onChange={handleSvgUpload} />
+      <input
+        type="text"
+        value={animationDuration}
+        onChange={(e) => setAnimationDuration(e.target.value)}
+        placeholder="Enter animation duration (e.g., 2s)"
+      />
+      <button onClick={downloadSVG}>Download SVG</button>
       <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
         <g dangerouslySetInnerHTML={{ __html: generateAnimation() }} />
       </svg>
