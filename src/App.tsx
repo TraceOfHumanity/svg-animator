aca24dadc76e630f986308cf05a447a69254279d
Alexander_Ovchar <azalexxx02@gmail.com>
2025-02-13 13:53:32 +0200

    Add SVG file upload and rendering functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import { MainContainer } from "./components/MainContainer"
-import { Title } from "./components/Title"
+import { MainContainer } from "./components/MainContainer";
+import { Title } from "./components/Title";
+import { useState } from "react";
 
 function App() {
+  const [svgs, setSvgs] = useState<string[]>([]);
+
+  const handleSvgUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const newSvgs: string[] = [];
+      Array.from(files).forEach((file) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          newSvgs.push(e.target?.result as string);
+          if (newSvgs.length === files.length) {
+            setSvgs((prevSvgs) => [...prevSvgs, ...newSvgs]);
+          }
+        };
+        reader.readAsText(file);
+      });
+    }
+  };
+
   return (
     <MainContainer>
       <Title />
+      <input type="file" accept="image/svg+xml" multiple onChange={handleSvgUpload} />
+      <div>
+        {svgs.map((svg, index) => (
+          <div key={index} dangerouslySetInnerHTML={{ __html: svg }} />
+        ))}
+      </div>
     </MainContainer>
-  )
+  );
 }
 
-export default App
+export default App;
